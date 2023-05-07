0ce12b3d2403802b957701a41a04c8e7291a31f5
Daniel Agg <daniel.agg@outlook.com>
2023-05-07 20:50:09 +0200

    rectangle iterator

--- src/shapes/rectangle.rs
+++ src/shapes/rectangle.rs
@@ -35,3 +35,42 @@ impl Area for Rectangle {
         return self.width * self.height;
     }
 }
+
+pub struct RectangleIterator {
+    points: Vec<(f64, f64)>,
+    index: usize,
+}
+
+impl Iterator for RectangleIterator {
+    type Item = (f64, f64);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.index >= self.points.len() {
+            return None;
+        }
+
+        let point = self.points[self.index];
+        self.index += 1;
+
+        return Some(point);
+    }
+}
+
+impl IntoIterator for Rectangle {
+    type Item = (f64, f64);
+    type IntoIter = RectangleIterator;
+
+    fn into_iter(self) -> Self::IntoIter {
+        let points = vec![
+            (self.x, self.y),
+            (self.x + self.width, self.y),
+            (self.x + self.width, self.y + self.height),
+            (self.x, self.y + self.height),
+        ];
+
+        return RectangleIterator {
+            points: points,
+            index: 0,
+        };
+    }
+}
